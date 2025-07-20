b08cdab53eda879e377c1106473671e41e86028a
Artyom Palvelev <no@example.com>
2025-07-19 22:57:09 -0700

    use static ffmpeg libraries

--- rust/build.rs
+++ rust/build.rs
@@ -1,18 +1,25 @@
 fn main() {
-    println!("cargo:rustc-link-search=native=../libavcodec");
-    println!("cargo:rustc-link-lib=avcodec");
-    println!("cargo:rustc-link-search=native=../libavformat");
-    println!("cargo:rustc-link-lib=avformat");
-    println!("cargo:rustc-link-search=native=../libavdevice");
-    println!("cargo:rustc-link-lib=avdevice");
     println!("cargo:rustc-link-search=native=../libavutil");
-    println!("cargo:rustc-link-lib=avutil");
+    println!("cargo:rustc-link-lib=static=avutil");
+
     println!("cargo:rustc-link-search=native=../libswresample");
-    println!("cargo:rustc-link-lib=swresample");
+    println!("cargo:rustc-link-lib=static=swresample");
+
     println!("cargo:rustc-link-search=native=../libswscale");
-    println!("cargo:rustc-link-lib=swscale");
+    println!("cargo:rustc-link-lib=static=swscale");
+
+    println!("cargo:rustc-link-search=native=../libavcodec");
+    println!("cargo:rustc-link-lib=static=avcodec");
+
+    println!("cargo:rustc-link-search=native=../libavformat");
+    println!("cargo:rustc-link-lib=static=avformat");
+
+    println!("cargo:rustc-link-search=native=../libavdevice");
+    println!("cargo:rustc-link-lib=static=avdevice");
+
     println!("cargo:rustc-link-search=native=../fftools");
-    println!("cargo:rustc-link-lib=fftools");
+    println!("cargo:rustc-link-lib=static=fftools");
+
     println!("cargo:rustc-link-lib=m");
     println!("cargo:rustc-link-lib=z");
     println!("cargo:rustc-link-lib=pthread");
